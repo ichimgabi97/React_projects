76a921de3758001aedc50a55a335642c8c9d672d
Ichim Gabriel <ichimgabi97@gmail.com>
2023-07-03 13:57:45 +0300

    update day 1

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,8 +1,17 @@
-function App() {
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
+const App = () => {
   return (
-    <div className="App">
-      OK
-    </div>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
